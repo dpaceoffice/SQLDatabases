92cf77a59b3062c5278afd79061ae8731d11cea2
dpaceoffice <dpaceoffice@gmail.com>
2021-04-11 17:17:41 -0500

    LD now correctly adds all tables, has all relationships, and the drops are in the correct order

--- ddl.sql
+++ ddl.sql
@@ -1,42 +1,71 @@
 -- Andy, David, Coral
 -- Spring 2021 
 -- CSCI 4152
-
-DROP TABLE skill;
-DROP TABLE position;
-DROP TABLE job;
-DROP TABLE gics;
-DROP TABLE course;
+--LD
+DROP TABLE p_works;
+DROP TABLE p_enrolls;
+DROP TABLE p_has;
+DROP TABLE teaches;
 DROP TABLE section;
+DROP TABLE c_req;
+DROP TABLE course;
+DROP TABLE requirement;
+DROP TABLE skill;
+DROP TABLE job;
+DROP TABLE payrate;
+DROP TABLE position;
+DROP TABLE address;
+DROP TABLE company;
 DROP TABLE person;
+DROP TABLE gics;
 
-DROP TABLE purchaseAZ;
+--GZ
+
+DROP TABLE purchaseaz;
+
 DROP TABLE sale;
+
 DROP TABLE inventory;
+
 DROP TABLE stocks;
+
 DROP TABLE store;
 
 DROP TABLE makes;
+
 DROP TABLE customer;
+
 DROP TABLE supplier;
+
 DROP TABLE purchaseline;
+
 DROP TABLE purchase;
+
 DROP TABLE lineitem;
+
 DROP TABLE contract;
+
 DROP TABLE product;
+
 DROP TABLE material;
+
 DROP TABLE searches;
+
 DROP TABLE factory;
 
-
+---------------------------------------------------------------------LD database
 CREATE TABLE gics (
     parent_id       NUMBER,
     industry_id     NUMBER,
     industry_title  VARCHAR(25),
     g_level         NUMBER,
     g_desc          VARCHAR(100),
+    
     CONSTRAINT gics_pk PRIMARY KEY ( parent_id,
-                                     industry_id )
+                                     industry_id ),
+    CONSTRAINT UK_PARENTID_GICS UNIQUE(parent_id),
+    CONSTRAINT UK_INDUSTRYID_GICS UNIQUE(industry_id)
+    
 );
 
 CREATE TABLE person (
@@ -46,6 +75,7 @@ CREATE TABLE person (
     email       VARCHAR(20),
     gender      NUMBER(1),
     phone       VARCHAR(20),
+    
     CONSTRAINT person_pk PRIMARY KEY ( person_id )
 );
 
@@ -54,26 +84,29 @@ CREATE TABLE company (
     industry_id      NUMBER,--forign, primary
     sub_industry_id  NUMBER,--forign, primary
     website          VARCHAR(20),
+    
     CONSTRAINT company_pk PRIMARY KEY ( company_id,
                                         industry_id,
                                         sub_industry_id ),
-    CONSTRAINT industry_fk FOREIGN KEY ( industry_id )
+    CONSTRAINT company_industry_fk FOREIGN KEY ( industry_id )
         REFERENCES gics ( parent_id ),
-    CONSTRAINT subindustry_fk FOREIGN KEY ( sub_industry_id )
-        REFERENCES gics ( industry_id )
+    CONSTRAINT company_subindustry_fk FOREIGN KEY ( sub_industry_id )
+        REFERENCES gics ( industry_id ),
+    CONSTRAINT UK_COMPANYID_COMPANY UNIQUE(company_id)
 );
 
 CREATE TABLE address (
     address_id  NUMBER,
-    company_id  NUMBER,
-    person_id   NUMBER,
+    company_id  NUMBER,--address can be for company
+    person_id   NUMBER,--address can also be for person 
     zip         NUMBER(5),
     street      VARCHAR(20),
     city        VARCHAR(20),
+    
     CONSTRAINT address_pk PRIMARY KEY ( address_id ),
-    CONSTRAINT company_fk FOREIGN KEY ( company_id )
+    CONSTRAINT address_company_fk FOREIGN KEY ( company_id )
         REFERENCES company ( company_id ),
-    CONSTRAINT person_fk FOREIGN KEY ( person_id )
+    CONSTRAINT address_person_fk FOREIGN KEY ( person_id )
         REFERENCES person ( person_id )
 );
 
@@ -91,6 +124,7 @@ CREATE TABLE payrate (
     pay_id  NUMBER,
     hourly  NUMBER,
     annual  NUMBER,
+    
     CONSTRAINT pay_pk PRIMARY KEY ( pay_id )
 );
 
@@ -99,14 +133,14 @@ CREATE TABLE job (
     position_id   NUMBER,
     company       VARCHAR(20),
     category_id   NUMBER,
-    employeemode  NUMBER(1),--full time or part time, multivalue boolean?
-    pay_type      NUMBER(1), -- Wage or Salary, multivalue boolean?
+    employeemode  NUMBER(1),--full time or part time, boolean?
+    pay_type      NUMBER(1), -- Wage or Salary, boolean?
     pay_rate      NUMBER, --derived, multivalue
 
     CONSTRAINT job_pk PRIMARY KEY ( job_id ),
-    CONSTRAINT position_fk FOREIGN KEY ( position_id )
+    CONSTRAINT job_position_fk FOREIGN KEY ( position_id )
         REFERENCES position ( position_id ),
-    CONSTRAINT payrate_fk FOREIGN KEY ( pay_rate )
+    CONSTRAINT job_payrate_fk FOREIGN KEY ( pay_rate )
         REFERENCES payrate ( pay_id )
 );
 
@@ -119,63 +153,126 @@ CREATE TABLE skill (
     CONSTRAINT skill_pk PRIMARY KEY ( skill_id )
 );
 
-CREATE TABLE requirment (
+CREATE TABLE requirement (
     req_id    NUMBER,--requirement id
     pos_id    NUMBER,--for position?
     job_id    NUMBER,--for job? 
     skill_id  NUMBER,--skill required
 
     CONSTRAINT req_pk PRIMARY KEY ( req_id ),
-    CONSTRAINT pos_fk FOREIGN KEY ( pos_id )
+    CONSTRAINT requirement_pos_fk FOREIGN KEY ( pos_id )
         REFERENCES position ( position_id ),
-    CONSTRAINT job_fk FOREIGN KEY ( job_id )
+    CONSTRAINT requirement_job_fk FOREIGN KEY ( job_id )
         REFERENCES job ( job_id ),
-    CONSTRAINT skill_fk FOREIGN KEY ( skill_id )
+    CONSTRAINT requirement_skill_fk FOREIGN KEY ( skill_id )
         REFERENCES skill ( skill_id )
 );
 
 CREATE TABLE course (
     course_id  NUMBER,
     title      VARCHAR(20),
-    level      VARCHAR(25),
+    c_level      VARCHAR(25),
     c_desc     VARCHAR(25),
     status     NUMBER(1),
+    
     CONSTRAINT course_pk PRIMARY KEY ( course_id )
+);
+
+CREATE TABLE c_req (
+    course_id      NUMBER,
+    req_course_id  NUMBER,
+    
+    CONSTRAINT c_req_pk PRIMARY KEY ( course_id,
+                                      req_course_id ),
+    CONSTRAINT c_req_fk FOREIGN KEY ( course_id )
+        REFERENCES course ( course_id ),
+    CONSTRAINT c_req_fk2 FOREIGN KEY ( req_course_id )
+        REFERENCES course ( course_id )
 );
 
 CREATE TABLE section (
     section_id  NUMBER,
     course_id   NUMBER,
-    s_year      CHAR(4),
-    c_date      VARCHAR(20),
+    s_year      CHAR(4),--year
+    c_date      VARCHAR(20),--dompletion date
     offering    VARCHAR(20),
     s_format    NUMBER(1),--online/class
     price       NUMBER,
-    CONSTRAINT section_pk PRIMARY KEY ( section_id ),
-    CONSTRAINT course_fk FOREIGN KEY ( course_id )
-        REFERENCES course ( course_id )
+    
+    CONSTRAINT section_pk PRIMARY KEY ( section_id,
+                                        s_year,
+                                        course_id ),
+    CONSTRAINT section_course_fk FOREIGN KEY ( course_id )
+        REFERENCES course ( course_id ),
+    CONSTRAINT UK_SECTIONID_SECTION UNIQUE(section_id)
 );
 
+CREATE TABLE teaches (
+    section_id  NUMBER,
+    skill_id    NUMBER,
+    
+    CONSTRAINT teach_pk PRIMARY KEY ( section_id,
+                                      skill_id ),
+    CONSTRAINT teaches_section_fk FOREIGN KEY ( section_id )
+        REFERENCES section ( section_id ),
+    CONSTRAINT teaches_skill_fk FOREIGN KEY ( skill_id )
+        REFERENCES skill ( skill_id )
+);
 
--------------------------------------------------------------------------------
+CREATE TABLE p_has (
+    person_id  NUMBER,
+    skill_id   NUMBER,
+    
+    CONSTRAINT person_has_pk PRIMARY KEY ( person_id,
+                                           skill_id ),
+    CONSTRAINT phas_person_fk FOREIGN KEY ( person_id )
+        REFERENCES person ( person_id ),
+    CONSTRAINT phas_skill_fk FOREIGN KEY ( skill_id )
+        REFERENCES skill ( skill_id )
+);
+
+CREATE TABLE p_enrolls (
+    person_id   NUMBER,
+    section_id  NUMBER,
+    
+    CONSTRAINT person_enrolls_pk PRIMARY KEY ( person_id,
+                                               section_id ),
+    CONSTRAINT penrolls_person_fk FOREIGN KEY ( person_id )
+        REFERENCES person ( person_id ),
+    CONSTRAINT penrolls_section_fk FOREIGN KEY ( section_id )
+        REFERENCES section ( section_id )
+);
+
+CREATE TABLE p_works (
+    person_id  NUMBER,
+    job_id     NUMBER,
+    
+    CONSTRAINT person_works_pk PRIMARY KEY ( person_id,
+                                             job_id ),
+    CONSTRAINT pworks_person_fk FOREIGN KEY ( person_id )
+        REFERENCES person ( person_id ),
+    CONSTRAINT pworks_job_fk FOREIGN KEY ( job_id )
+        REFERENCES job ( job_id )
+);
+
+-----------------------------------------------------------------------------GZ
 CREATE TABLE store (
     storeid  NUMBER,
     address  VARCHAR(20),
     zipcode  NUMBER,
     phone    NUMBER,
-
-    CONSTRAINT store_pk PRIMARY KEY (storeid)
+    CONSTRAINT store_pk PRIMARY KEY ( storeid )
 );
 
-CREATE TABLE stocks(
+CREATE TABLE stocks (
     storeid  NUMBER,
     itemnum  NUMBER,
-
     CONSTRAINT stocks_fk FOREIGN KEY ( storeid )
         REFERENCES store ( storeid ),
     CONSTRAINT stocks_fk FOREIGN KEY ( itemnum )
         REFERENCES inventory ( itemnum )
 );
+
 CREATE TABLE inventory (
     itemnum      NUMBER,
     title        VARCHAR(20),
@@ -185,10 +282,10 @@ CREATE TABLE inventory (
     avgcost      NUMBER,
     shelfcode    NUMBER,
     minlevel     NUMBER,
-
-    CONSTRAINT inventory_pk PRIMARY KEY (itemnum, 
-        quantity, avgcost, minlevel)
-
+    CONSTRAINT inventory_pk PRIMARY KEY ( itemnum,
+                                          quantity,
+                                          avgcost,
+                                          minlevel )
 );
 
 CREATE TABLE sale (
@@ -199,33 +296,31 @@ CREATE TABLE sale (
     price       NUMBER,
     note        varchar,
     minlevel    NUMBER,
-
-    CONSTRAINT sale_pk PRIMARY KEY (invoicenbr),
+    CONSTRAINT sale_pk PRIMARY KEY ( invoicenbr ),
     CONSTRAINT sale_fk FOREIGN KEY ( itemnbr )
         REFERENCES inventory ( itemnum ),
     CONSTRAINT sale_fk FOREIGN KEY ( quantity )
         REFERENCES inventory ( quantity ),
     CONSTRAINT sale_fk FOREIGN KEY ( minlevel )
-        REFERENCES inventory ( minlevel ),
+        REFERENCES inventory ( minlevel )
 );
 
-CREATE TABLE purchaseAZ (
+CREATE TABLE purchaseaz (
     puchasenbr  NUMBER,
     p_date      DATE,
     itemnbr     NUMBER,
     quantity    NUMBER,
     unitcost    NUMBER,
-    note        VARCHAR(20)
-
-    CONSTRAINT purchaseAZ_pk PRIMARY KEY (puchasenbr),
+    note        VARCHAR(20),
+    CONSTRAINT purchaseaz_pk PRIMARY KEY ( puchasenbr ),
     CONSTRAINT purchase_fk FOREIGN KEY ( itemnbr )
         REFERENCES inventory ( itemnum ),
-    CONSTRAINT purchaseAZ_fk FOREIGN KEY ( quantity )
+    CONSTRAINT purchaseaz_fk FOREIGN KEY ( quantity )
         REFERENCES inventory ( quantity ),
-    CONSTRAINT purchaseAZ_fk FOREIGN KEY ( unitcost )
+    CONSTRAINT purchaseaz_fk FOREIGN KEY ( unitcost )
         REFERENCES inventory ( avgcost )
 );
------------------------------------------------------------------------
+------------------------------------------------------------------------------AZ
 CREATE TABLE factory (
     factoryid    NUMBER,
     factoryname  VARCHAR(50),
@@ -233,13 +328,12 @@ CREATE TABLE factory (
     zipcode      NUMBER,
     phone        NUMBER,
     manager      VARCHAR(20),
-
-    CONSTRAINT factory_pk PRIMARY KEY (factoryid),
+    CONSTRAINT factory_pk PRIMARY KEY ( factoryid )
 );
-CREATE TABLE searches(
-    factoryid    NUMBER,
-    materialcode  NUMBER,
 
+CREATE TABLE searches (
+    factoryid     NUMBER,
+    materialcode  NUMBER,
     CONSTRAINT searches_fk FOREIGN KEY ( factoryid )
         REFERENCES factory ( factoryid ),
     CONSTRAINT searches_fk FOREIGN KEY ( materialcode )
@@ -252,8 +346,9 @@ CREATE TABLE material (
     quantity      NUMBER,
     unit          NUMBER,
     minlevel      NUMBER,
-
-    CONSTRAINT material_pk PRIMARY KEY (materialcode,quantity)
+    CONSTRAINT material_pk PRIMARY KEY ( materialcode,
+                                         quantity )
+);
 
 CREATE TABLE product (
     productcode  NUMBER,
@@ -262,8 +357,9 @@ CREATE TABLE product (
     quantity     NUMBER,
     unit         NUMBER,
     avgcost      NUMBER,
-
-    CONSTRAINT product_pk PRIMARY KEY (productcode, quantity)
+    CONSTRAINT product_pk PRIMARY KEY ( productcode,
+                                        quantity )
+);
 
 CREATE TABLE contract (
     contractid   NUMBER,
@@ -271,18 +367,15 @@ CREATE TABLE contract (
     c_date       DATE,
     saleamount   NUMBER,
     payschedule  DATE,
-
-    CONSTRAINT contract_pk PRIMARY KEY (contractid),
+    CONSTRAINT contract_pk PRIMARY KEY ( contractid ),
     CONSTRAINT contract_fk FOREIGN KEY ( customerid )
         REFERENCES customer ( companyid )
-
 );
 
 CREATE TABLE lineitem (
     contractid   NUMBER,
     productcode  NUMBER,
-    quantity     NUMBER
-
+    quantity     NUMBER,
     CONSTRAINT lineitem_fk FOREIGN KEY ( contractid )
         REFERENCES contract ( contractid ),
     CONSTRAINT lineitem_fk FOREIGN KEY ( productcode )
@@ -298,8 +391,7 @@ CREATE TABLE purchase (
     bookdate          DATE,
     paydate           DATE,
     note              VARCHAR(20),
-
-    CONSTRAINT purchase_pk PRIMARY KEY (purchasenum),
+    CONSTRAINT purchase_pk PRIMARY KEY ( purchasenum ),
     CONSTRAINT purchase_fk FOREIGN KEY ( supplierid )
         REFERENCES supplier ( companyid )
 );
@@ -308,7 +400,6 @@ CREATE TABLE purchaseline (
     purchasenum   NUMBER,
     materialcode  NUMBER,
     quantity      NUMBER,
-
     CONSTRAINT purchaseline_fk FOREIGN KEY ( purchasenum )
         REFERENCES purchase ( purchasenum ),
     CONSTRAINT purchaseline_fk FOREIGN KEY ( materialcode )
@@ -321,23 +412,20 @@ CREATE TABLE supplier (
     companyid     NUMBER,
     website       VARCHAR(50),
     contactemail  VARCHAR(50),
-
-    CONSTRAINT supplier_pk PRIMARY KEY (companyid),
+    CONSTRAINT supplier_pk PRIMARY KEY ( companyid )
 );
 
 CREATE TABLE customer (
     companyid      NUMBER,
     contactperson  VARCHAR(20),
     contactemail   VARCHAR(50),
-
-    CONSTRAINT customer_pk PRIMARY KEY (companyid),
+    CONSTRAINT customer_pk PRIMARY KEY ( companyid )
 );
 
 CREATE TABLE makes (
     factoryid    NUMBER,
     productcode  NUMBER,
     quantity     NUMBER,
-
     CONSTRAINT makes_fk FOREIGN KEY ( factoryid )
         REFERENCES factory ( factoryid ),
     CONSTRAINT makes_fk FOREIGN KEY ( productcode )
