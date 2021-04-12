b0b5cfdbc0f084fb4a9e53e1a893875b8e83805e
Andy T <73973372+Dekaid@users.noreply.github.com>
2021-04-12 00:27:20 -0500

    added inserts for each tables and need to be fill

---
+++ sql/inserts.sql
@@ -0,0 +1,192 @@
+-- TODO: Fill the inserts, need about 3 to 4 inserts per table
+INSERT INTO gics VALUES
+INSERT INTO gics VALUES
+INSERT INTO gics VALUES
+INSERT INTO gics VALUES
+
+commit;
+
+INSERT INTO person VALUES
+INSERT INTO person VALUES
+INSERT INTO person VALUES
+INSERT INTO person VALUES
+
+commit;
+
+INSERT INTO company VALUES
+INSERT INTO company VALUES
+INSERT INTO company VALUES
+INSERT INTO company VALUES
+
+commit;
+
+INSERT INTO address VALUES
+INSERT INTO address VALUES
+INSERT INTO address VALUES
+INSERT INTO address VALUES
+
+commit;
+
+INSERT INTO position VALUES
+INSERT INTO position VALUES
+INSERT INTO position VALUES
+INSERT INTO position VALUES
+
+commit;
+
+INSERT INTO payrate VALUES
+INSERT INTO payrate VALUES
+INSERT INTO payrate VALUES
+INSERT INTO payrate VALUES
+
+commit;
+
+INSERT INTO job VALUES
+INSERT INTO job VALUES
+INSERT INTO job VALUES
+INSERT INTO job VALUES
+
+commit;
+
+INSERT INTO skil VALUES
+INSERT INTO skil VALUES
+INSERT INTO skil VALUES
+INSERT INTO skil VALUES
+
+commit;
+
+INSERT INTO requirement VALUES
+INSERT INTO requirement VALUES
+INSERT INTO requirement VALUES
+INSERT INTO requirement VALUES
+
+commit;
+
+INSERT INTO course VALUES
+INSERT INTO course VALUES
+INSERT INTO course VALUES
+INSERT INTO course VALUES
+
+commit;
+
+INSERT INTO section VALUES
+INSERT INTO section VALUES
+INSERT INTO section VALUES
+INSERT INTO section VALUES
+
+commit;
+
+---------------------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO store VALUES (1, '608 Joe Mama', 70072, '504-988-4742');
+INSERT INTO store VALUES (2, '897 Maydu Street', 70078, '504-988-8975');
+INSERT INTO store VALUES (3, '625 Lema Avenue', 70052, '504-988-6563');
+INSERT INTO store VALUES (4, '879 Ligma Court', 70089, '504-988-3465');
+INSERT INTO store VALUES (5, '542 Stella Avenue', 70069, '504-988-5987');
+
+commit;
+
+INSERT INTO stocks VALUES
+INSERT INTO stocks VALUES
+INSERT INTO stocks VALUES
+INSERT INTO stocks VALUES
+
+commit;
+
+INSERT INTO inventory VALUES
+INSERT INTO inventory VALUES
+INSERT INTO inventory VALUES
+INSERT INTO inventory VALUES
+
+commit;
+
+INSERT INTO sale VALUES
+INSERT INTO sale VALUES
+INSERT INTO sale VALUES
+INSERT INTO sale VALUES
+
+commit;
+
+INSERT INTO purchaseAZ VALUES
+INSERT INTO purchaseAZ VALUES
+INSERT INTO purchaseAZ VALUES
+INSERT INTO purchaseAZ VALUES
+
+commit;
+----------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO factory VALUES
+INSERT INTO factory VALUES
+INSERT INTO factory VALUES
+INSERT INTO factory VALUES
+
+commit;
+
+INSERT INTO searches VALUES
+INSERT INTO searches VALUES
+INSERT INTO searches VALUES
+INSERT INTO searches VALUES
+
+commit;
+
+INSERT INTO material VALUES
+INSERT INTO material VALUES
+INSERT INTO material VALUES
+INSERT INTO material VALUES
+
+commit;
+
+INSERT INTO product VALUES
+INSERT INTO product VALUES
+INSERT INTO product VALUES
+INSERT INTO product VALUES
+
+commit;
+
+INSERT INTO contract VALUES
+INSERT INTO contract VALUES
+INSERT INTO contract VALUES
+INSERT INTO contract VALUES
+
+commit;
+
+INSERT INTO lineitem VALUES
+INSERT INTO lineitem VALUES
+INSERT INTO lineitem VALUES
+INSERT INTO lineitem VALUES
+
+commit;
+
+INSERT INTO purhcase VALUES
+INSERT INTO purhcase VALUES
+INSERT INTO purhcase VALUES
+INSERT INTO purhcase VALUES
+
+commit;
+
+INSERT INTO purhcaseline VALUES
+INSERT INTO purhcaseline VALUES
+INSERT INTO purhcaseline VALUES
+INSERT INTO purhcaseline VALUES
+
+commit;
+
+INSERT INTO supplier VALUES
+INSERT INTO supplier VALUES
+INSERT INTO supplier VALUES
+INSERT INTO supplier VALUES
+
+commit;
+
+INSERT INTO customer VALUES
+INSERT INTO customer VALUES
+INSERT INTO customer VALUES
+INSERT INTO customer VALUES
+
+commit;
+
+INSERT INTO makes VALUES
+INSERT INTO makes VALUES
+INSERT INTO makes VALUES
+INSERT INTO makes VALUES
+
+commit;
