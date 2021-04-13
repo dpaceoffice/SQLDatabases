4ba1c786e8c3d74a276e0771341cc4c3f68f301a
dpaceoffice <dpaceoffice@gmail.com>
2021-04-12 22:09:49 -0500

    Fixed a bug with the processing

--- tools/insertCompanies.py
+++ tools/insertCompanies.py
@@ -5,13 +5,16 @@ file = open("./tools/materials/indeed100.txt")
 data = file.readlines()
 count = 0
 for line in data:
-    line = line.split(',')
-    company_column = line[3]
-    web_column = line[2]
-    summary_column = line[7]
-    cname = company_column[company_column.find("company="):company_column.find(",")]
-    website = web_column[web_column.find("href="):web_column.find(",")]
-    cdesc = summary_column[summary_column.find("summary="):summary_column.find("]")]
+
+    cnameIndex = line.find("company=")
+    cname = line[cnameIndex:line.find(",", cnameIndex)]
+
+    webIndex = line.find("href=")
+    website = line[webIndex:line.find(",", webIndex)]
+
+    sumIndex = line.find("summary=")
+    cdesc = line[sumIndex:line.find("]", sumIndex)]
+
     industryId = '451020'
     sub_industryId = '45102010'
     string = 'INSERT INTO company VALUES ('+str(count)+', \''+cname.strip("company=")+'\', \''+cdesc.strip("summary=")+'\', '+str(industryId)+', '+str(sub_industryId)+', \''+website.strip("website=")+'\');'
