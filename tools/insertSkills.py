0fb991b7e40238eeaf95ed2d8e3a2d7174037c23
dpaceoffice <dpaceoffice@gmail.com>
2021-04-21 21:44:03 -0500

    Modified to include a made up description

--- tools/insertSkills.py
+++ tools/insertSkills.py
@@ -1,7 +1,7 @@
 from csv import reader
 # open file in read mode
 
-with open('skill270.csv', 'r') as read_obj:
+with open('tools\materials\skill270.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
     # Iterate over each row in the csv using reader object
@@ -10,8 +10,8 @@ with open('skill270.csv', 'r') as read_obj:
         # row variable is a list that represents a row in csv
         skill_id = count
         title = row
-        s_desc = 'NULL'
+        s_desc = 'Skill id: '+str(skill_id)
         s_level = 0
-        string = 'INSERT INTO skill VALUES ('+str(skill_id)+', \''+str(title[0])+'\', \''+s_desc+'\', \''+str(s_level)+'\');'
-        print(string).replace('\'','')
+        string = 'INSERT INTO skill VALUES ('+str(skill_id)+', \''+str(title[0])+'\', \''+s_desc+'\', '+str(s_level)+');'
+        print(string)
         count += 1
