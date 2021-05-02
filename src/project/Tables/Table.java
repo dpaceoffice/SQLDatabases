ec8d2c2b54fe05c2cbe44cdd6989f97edc4c3081
dpaceoffice <dpaceoffice@gmail.com>
2021-05-02 13:37:38 -0500

    Abstract class for Tables

---
+++ src/project/Tables/Table.java
@@ -0,0 +1,15 @@
+package project.Tables;
+
+import java.sql.Connection;
+import java.util.HashMap;
+
+public abstract class Table {
+
+    public HashMap<Integer, Object> rows;
+
+    public abstract boolean fetch(Connection con);
+
+
+    public abstract boolean delete(Connection con);
+
+}
