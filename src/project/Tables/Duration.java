2fa3c2fa1c12b3fc3d93bdd3ae8d1de45ab95015
dpaceoffice <dpaceoffice@gmail.com>
2021-05-02 13:37:08 -0500

    Modified for null dates

---
+++ src/project/Tables/Duration.java
@@ -0,0 +1,36 @@
+package project.Tables;
+
+import java.sql.Date;
+
+public class Duration {
+
+    private java.util.Date startDate, endDate;
+    
+    public Duration(Date startDate, Date endDate) {
+        if(startDate == null)
+            this.startDate = new java.util.Date();
+        else
+            this.startDate = new java.util.Date(startDate.getTime());
+        if(endDate == null)
+            this.endDate = new java.util.Date();
+        else
+            this.endDate = new java.util.Date(endDate.getTime());
+    }
+    
+    public java.util.Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(java.util.Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public java.util.Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(java.util.Date endDate) {
+        this.endDate = endDate;
+    }
+
+}
